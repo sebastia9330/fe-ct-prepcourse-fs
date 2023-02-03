e77187d1f869b1d0a1430b6148fbf129c08af6e4
sebastian carrero <mugssebastian@gmail.com>
2023-02-02 20:29:40 -0500

    homework

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,8 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arreglos = Object.entries(objeto);
+   return arreglos
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +16,8 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var nuevoArray = string.split('');
+   
 }
 
 function capToFront(string) {
